037459dca48324d4cf36f4c491af3886ab2df599
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-20 12:31:33 +0530

    refactor: move it to a style reg

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,1 +1,5 @@
-export const Layout = ({children})=><div class="max-w-4xl mx-auto flex flex-col m-10 min-h-[100vh] gap-3">{children}</div>
+export const Layout = ({ children }) => (
+  <div class="max-w-4xl mx-auto flex flex-col m-10 min-h-[100vh] gap-3">
+    {children}
+  </div>
+);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,89 @@
 import { Layout } from "~/components/layout";
-import { Button } from "~/components/shared/button";
+import { styles } from "~/components/shared/style-registry";
+import { cn } from "~/lib/cn";
+import { cva } from "class-variance-authority";
+import { h } from "preact";
 
 export default () => {
   return (
     <Layout>
-      <h1 class="font-semibold">ZincUI</h1>
+      <hgroup>
+        <h1 class="font-semibold">ZincUI</h1>
+        <p>Tailwind Styled HTML Primitives for the rest of the world</p>
+      </hgroup>
       <section>
-        <h2>Components</h2>
-        <Button>Hello</Button>
-        <pre>{Button.story}</pre>
+        {styles.map((d) => {
+          const classVariantBuilder = cva(d.base, d.variants);
+          const variants = Object.keys(d.variants.variants);
+          const variantProperties = variants.reduce((acc, varKey) => {
+            acc[varKey] = Object.keys(d.variants.variants[varKey]).sort();
+            return acc;
+          }, {});
+          return (
+            <div>
+              <h2 class="font-bold">{d.name} Variants</h2>
+              <h3 class="font-semibold">Default</h3>
+              {d.forElements.map((el) => {
+                return (
+                  <div class="mt-1">
+                    {h(
+                      el,
+                      {
+                        class: cn(classVariantBuilder({})),
+                      },
+                      "Sample Text"
+                    )}
+                  </div>
+                );
+              })}
+              {variants.map((variantKey) => {
+                return (
+                  <div class="flex flex-col gap-2 mt-10">
+                    <h3 class="font-semibold">
+                      {variantKey}: [
+                      {variantProperties[variantKey]
+                        .filter((innerKey) => innerKey !== "default")
+                        .join(",")}
+                      ]{" "}
+                    </h3>
+                    <div class="flex gap-2 mt-1">
+                      {d.forElements.map((el) => {
+                        return variantProperties[variantKey]
+                          .filter((innerKey) => innerKey !== "default")
+                          .map((keyProp) => {
+                            if (keyProp == "icon") {
+                              return h(
+                                el,
+                                {
+                                  class: cn(
+                                    classVariantBuilder({
+                                      [variantKey]: keyProp,
+                                    })
+                                  ),
+                                },
+                                "▲"
+                              );
+                            }
+                            return h(
+                              el,
+                              {
+                                class: cn(
+                                  classVariantBuilder({
+                                    [variantKey]: keyProp,
+                                  })
+                                ),
+                              },
+                              "Sample Text"
+                            );
+                          });
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
       </section>
     </Layout>
   );
